aef172bac84dc6a8d6a34e725f2a5bfdb86e3876
Erik Ferrari <eferrari@datacolor.com>
2025-10-22 22:13:08 +0200

    Add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Packages from "@/components/Packages";
 import CallToAction from "@/components/CallToAction";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <About />
       <Packages />
       <CallToAction />
+      <Footer />
     </main>
   );
 }
