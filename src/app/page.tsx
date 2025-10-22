c84310eef194a1444193d420b3425513e0a801b0
Erik Ferrari <eferrari@datacolor.com>
2025-10-22 23:40:00 +0200

    Remove feature in the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from "@/components/Hero";
 import About from "@/components/About";
-import Features from "@/components/Features";
 import Packages from "@/components/Packages";
 import CallToAction from "@/components/CallToAction";
 import Footer from "@/components/Footer";
@@ -10,7 +9,6 @@ export default function Home() {
     <main className="min-h-screen bg-white">
       <Hero />
       <About />
-      <Features />
       <Packages />
       <CallToAction />
       <Footer />
